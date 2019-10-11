839f995dbef402768b8ca9d9ce23446d953d4b6d
15611 <15611744558@163.com>
2019-10-11 18:50:39 +0800

    1.配置数据库 2.添加用户实体类

--- demo/src/main/java/com/example/messageBoard/common/ZipUtil.java
+++ demo/src/main/java/com/example/messageBoard/common/ZipUtil.java
@@ -1,4 +1,4 @@
-package com.example.demo.base;
+package com.example.messageBoard.common;
 
 public class ZipUtil {
 
---
+++ demo/src/main/java/com/example/messageBoard/entity/User.java
@@ -0,0 +1,70 @@
+package com.example.messageBoard.entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private String name;//姓名
+    private String gender;//性别
+    private String passWord;//密码
+    private String phone;//手机号
+    private String email;//邮箱
+    private String number;//身份证号
+    private String accountNumber;//账号
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+}
